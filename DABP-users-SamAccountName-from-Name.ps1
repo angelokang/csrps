fe3464ad4fc2df6e9b0e6f7477da735b12b4eedc
angelokang <118285114+angelokang@users.noreply.github.com>
2023-03-22 08:12:19 -0400

    via upload by drag and drop

---
+++ DABP-users-SamAccountName-from-Name.ps1
@@ -0,0 +1,43 @@
+﻿Install-Module -Name ImportExcel -Scope CurrentUser
+# Import the ImportExcel module
+Import-Module ImportExcel
+
+# Set the path to your Excel file
+$excelFilePath = "C:\users\kangah\Downloads\Copy of Copy of DABP  DPPS Staffing List as of 3-16-23.xlsx"
+
+# Import the data from the Excel file
+$excelData = Import-Excel -Path $excelFilePath
+
+# Retrieve the 3rd column data
+$thirdColumnData = $excelData | Select-Object -Property "Employee Name"
+
+# Output the 3rd column data
+$thirdColumnData
+
+$outputData = $thirdColumnData
+# Remove the header lines and split the output into an array of lines
+$employeeLines = ($outputData -split "`n" | Select-Object -Skip 2).Trim()
+$employees = $employeeLines | ForEach-Object {
+    # Split each line by comma and trim whitespace
+    $splitData = $_ -split ', ' | ForEach-Object { $_.Trim() }
+
+    # Create a custom object with separated lastname and firstname
+    [PSCustomObject]@{
+        Lastname  = $splitData[0]
+        Firstname = $splitData[1]
+    }
+}
+
+# Output the employees with separated lastname and firstname
+$employees
+
+
+$fullnames = $thirdColumnData
+foreach($name in $fullnames){
+    $splitname = $name -split ', ' |ForEach-Object {$_.Trim()}
+
+    $surname = $splitname[0]
+    $givenname = $splitname[1]
+    $user = Get-ADUser -Filter{(givenname -eq $givenname) -and (surname -eq $surname)}
+    $user.SamAccountName
+}
---
+++ DPPS-users-SamAccountName-from-Name.ps1
@@ -0,0 +1,20 @@
+﻿Install-Module -Name ImportExcel -Scope CurrentUser
+
+
+# Set the path to your Excel file
+$excelFilePath = "C:\users\kangah\Downloads\Copy of Copy of DABP  DPPS Staffing List as of 3-16-23.xlsx"
+
+# Set the name or index of the sheet to import data from (zero-based index)
+#$sheetIndex = 0
+$sheetName = "DPPS" # Replace with the name of the 2nd sheet
+# Import the data from the 2nd sheet of the Excel file
+#$excelData = Import-Excel -Path $excelFilePath -WorksheetIndex $sheetIndex
+$excelData = Import-Excel -Path $excelFilePath -WorksheetName $sheetName
+
+# Retrieve the 3rd column data
+$thirdColumnIndex = 2 # Index of the 3rd column (zero-based)
+$thirdColumnName = ($excelData | Get-Member -MemberType NoteProperty)[$thirdColumnIndex].Name
+$thirdColumnData = $excelData | Select-Object -Property $thirdColumnName
+
+# Output the 3rd column data
+$thirdColumnData
